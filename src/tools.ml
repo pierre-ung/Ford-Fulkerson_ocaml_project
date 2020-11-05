9d4c28e963f31cc169f360a9db0afb6e847b2b84
Pierre Ung <pierre.ung195@gmail.com>
2020-11-05 10:13:48 +0100

    tools.ml base

--- src/tools.ml
+++ src/tools.ml
@@ -1,5 +1,4 @@
 let clone_nodes gr = List.map (fun (a, b) -> a) gr
 let gmap gr f = List.map (fun (a, b) -> (a, (List.map f b))) gr
 let rec add_arc g id1 id2 n =
-  match g with 
-  | 
+  assert false 

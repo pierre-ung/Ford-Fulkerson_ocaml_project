3551d3d4689845f1e06eff3e1caafd4b91e5e850
RobertL31 <loicalban@laposte.net>
2020-11-17 21:29:17 +0100

    find_path + add_path

--- src/ford_fulkerson.ml
+++ src/ford_fulkerson.ml
@@ -2,21 +2,39 @@ open Graph
 open Tools
 
 
-let init_graph gr = 
+let init_graph (gr:int graph) = 
   gmap gr (fun x -> (0,x))
 
-let residual_graph gr =
+let residual_graph (gr:(int*int) graph) =
   gmap gr (fun (x,y) -> y-x)
 
 
+(* on fera tourner l'algo jusqu'à ce que la tete de la blacklist soit la source*)
+let find_path (gr: int graph) (src: id) (target: id) (blacklist: id list) = 
+  let rec loop (actual: id) (path: id list) (min_weight: int) (potentials: int out_arcs) = 
+  match potentials with
+    | [] -> (actual::blacklist,[],0)
+    | (next,weight)::tail -> if next = target
+                             then (blacklist, target::path, (min weight min_weight))
+                            else
+                              if not (List.mem next blacklist)
+                              then if weight > 0 
+                                    then loop next (next::path) (min weight min_weight) (out_arcs gr next)
+                                    else loop actual path min_weight tail
+                              else loop actual path min_weight tail                                
+  in loop src [src] max_int (out_arcs gr src)
 
-(*principe de all_path:
-  match les arcs de sortie
-  pour chaque noeud vérifie s'il est dans mémo
-  si non, on all_path à partir de ce noeud, ajoute le résultat flattené au all_path des autres noeuds.
-  Normalement, pas besoin de fonction intermédiaire.
-*)
 
+let rec add_path (gr: (int*int) graph) (path: id list) (n: int) = 
+  match path with
+  | [target] -> gr
+  | actual::next::tail -> let new_graph = add_arc gr actual next n
+                          in add_path new_graph (next::tail) n
+
+
+
+
+(*)
 let all_path (gr: 'a graph) (src: id) (trg: id) =
 
   let rec aux (actual: id) (memo: id list) (path: 'a graph) (out: 'a out_arcs) = 
@@ -31,24 +49,4 @@ let all_path (gr: 'a graph) (src: id) (trg: id) =
       else 
         (List.flatten(aux node (node::memo) (new_arc path actual node w) (out_arcs gr node)))@(List.flatten (aux actual memo path t))
   in aux src [src] (clone_nodes gr) (out_arcs gr src)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*)
--- src/ford_fulkerson.mli
+++ src/ford_fulkerson.mli
@@ -5,6 +5,6 @@ val init_graph: int graph -> (int*int) graph
 
 val residual_graph: (int*int) graph -> int graph
 
-val find_path: (int) graph -> id -> id -> (id list*int) 
+val find_path: int graph -> id -> id -> id list -> ((id list)*(id list)*int)
 
-val opt_graph: (int*int) graph -> (id list*int) -> ((int*int) graph, bool)
+val add_path: (int*int) graph -> id list -> int -> (int*int) graph
